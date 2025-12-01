a0351a553ddf359e8bc1f85a8ef94dfa72e83e7f
09acosta09-dot <09acosta09@gmail.com>
2025-12-01 12:47:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,7 @@ from telegram.ext import (
 # ----------------------------
 
 # Toma el token de una variable de entorno BOT_TOKEN (configúrala en Railway)
-TOKEN = os.getenv("7668998247:AAECr_Y1sk6P2uOWkw6ZoJMPdmT_EBksAcA", "PON_AQUI_TU_TOKEN_SI_NO_USAS_ENV")
-
+TOKEN = os.getenv("7668998247:AAECr_Y1sk6P2uOWkw6ZoJMPdmT_EBksAcA"
 # SOLO estos IDs pueden ver el menú de administrador
 ADMIN_IDS = [1741298723, 7076796229]
 
