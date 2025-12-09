922e0560e02f8b7694844f3dbaf4b5f49247f4f1
09acosta09-dot <09acosta09@gmail.com>
2025-12-09 14:08:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -419,9 +419,8 @@ for admin_id in ADMIN_IDS:
                 logger.error(f"No se pudo avisar a admin {admin_id}: {e}")
 
         return True
-
-    return False
-
+           except:
+             return False
 
 # ---------------------------
 # MANEJO DE TEXTO GENERAL
